a9e1cdff8deb6c21a16b2a0e99e9c4b8fd552716
Denis <denis.coroliov@gmail.com>
2024-04-10 14:49:56 +0300

    cache, in-memoryC - fix tests

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -5,6 +5,7 @@ import { PostsController } from './posts.controller';
 import ormConfigTest from '../config/orm.config.test';
 import { PostEntity } from './entities/post.entity';
 import { PostsService } from './services/posts.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('PostsController', () => {
   let controller: PostsController;
@@ -14,6 +15,11 @@ describe('PostsController', () => {
       controllers: [PostsController],
       providers: [PostsService],
       imports: [
+        CacheModule.register({
+          ttl: 5 * 60 * 1000, // we have @nestjs/cache-manager@5 , it's milliseconds
+          // max: 10, // maximum number of items in cache
+          isGlobal: true,
+        }),
         TypeOrmModule.forRootAsync({
           useFactory: ormConfigTest,
         }),
